f80acd3dab18929e23fe879d170d9a10d3245407
mcdchristian <mcdchristian4@gmail.com>
2026-08-20 16:56:02 +0200

    feat: add request ID tracking middleware

---
+++ src/middleware/requestId.js
@@ -0,0 +1,7 @@
+const requestId = (req, res, next) => {
+  req.id = req.headers['x-request-id'] || crypto.randomUUID();
+  res.setHeader('X-Request-ID', req.id);
+  next();
+};
+
+module.exports = requestId;
